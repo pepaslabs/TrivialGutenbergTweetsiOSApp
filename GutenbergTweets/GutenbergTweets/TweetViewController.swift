e47ce322cdcdaeeb73268295d8c9f71d2b51b5f5
Jason Pepas <jasonpepas@gmail.com>
2015-12-06 15:52:08 -0600

    working on StyleModel

--- GutenbergTweets/GutenbergTweets/TweetViewController.swift
+++ GutenbergTweets/GutenbergTweets/TweetViewController.swift
@@ -8,6 +8,47 @@
 
 import UIKit
 
+protocol UIViewStyleModelProtocol
+{
+    var backgroundColor: UIColor { get }
+}
+
+protocol UILabelStyleModelProtocol: UIViewStyleModelProtocol
+{
+    var font: UIFont { get }
+    var textColor: UIColor { get }
+}
+
+struct UIViewStyleModel: UIViewStyleModelProtocol
+{
+    let backgroundColor: UIColor
+}
+
+struct UILabelStyleModel: UILabelStyleModelProtocol
+{
+    let backgroundColor: UIColor
+    let font: UIFont
+    let textColor: UIColor
+}
+
+extension UIView
+{
+    func applyStyleModel(model: UIViewStyleModelProtocol)
+    {
+        backgroundColor = model.backgroundColor
+    }
+}
+
+extension UILabel
+{
+    func applyStyleModel(model: UILabelStyleModelProtocol)
+    {
+        font = model.font
+        backgroundColor = model.backgroundColor
+        textColor = model.textColor
+    }
+}
+
 class TweetView: UIView
 {
     @IBOutlet weak var nameLabel: UILabel!
@@ -20,16 +61,21 @@ class TweetView: UIView
     @IBOutlet weak var trailingMarginConstraint: NSLayoutConstraint!
 
     @IBOutlet var verticalSpacingConstraints: [NSLayoutConstraint]!
+
+    struct LayoutModel
+    {
+        let topMarginConstant: CGFloat = 16
+        let bottomMarginConstant: CGFloat = 8
+        let leadingMarginConstant: CGFloat = 8
+        let trailingMarginConstant: CGFloat = 8
+
+        let verticalSpacingConstant: CGFloat = 8
+    }
     
     var layoutModel: LayoutModel = LayoutModel() {
         didSet {
             _applyLayoutModel(layoutModel)
         }
-    }
-    
-    override func updateConstraints() {
-        super.updateConstraints()
-        _applyLayoutModel(layoutModel)
     }
     
     private func _applyLayoutModel(model: LayoutModel)
@@ -44,15 +90,66 @@ class TweetView: UIView
             constraint.constant = model.verticalSpacingConstant
         }
     }
+
+    override func updateConstraints() {
+        super.updateConstraints()
+        _applyLayoutModel(layoutModel)
+    }
     
-    struct LayoutModel
+    struct StyleModel
     {
-        let topMarginConstant: CGFloat = 16
-        let bottomMarginConstant: CGFloat = 8
-        let leadingMarginConstant: CGFloat = 8
-        let trailingMarginConstant: CGFloat = 8
+        let viewStyle: UIViewStyleModel = UIViewStyleModel(backgroundColor: UIColor.whiteColor())
         
-        let verticalSpacingConstant: CGFloat = 8
+        let nameLabelStyle: UILabelStyleModel = UILabelStyleModel(
+            backgroundColor: UIColor.whiteColor(),
+            font: UIFont.systemFontOfSize(18),
+            textColor: UIColor.darkGrayColor())
+        
+        let dateLabelStyle: UILabelStyleModel = UILabelStyleModel(
+            backgroundColor: UIColor.whiteColor(),
+            font: UIFont.systemFontOfSize(14),
+            textColor: UIColor.darkGrayColor())
+        
+        let bodyLabelStyle: UILabelStyleModel = UILabelStyleModel(
+            backgroundColor: UIColor.whiteColor(),
+            font: UIFont.systemFontOfSize(14),
+            textColor: UIColor.darkGrayColor())
+    }
+    
+    var styleModel: StyleModel = StyleModel() {
+        didSet {
+            _applyStyleModel(styleModel)
+        }
+    }
+    
+    private func _applyStyleModel(model: StyleModel)
+    {
+        applyStyleModel(model.viewStyle)
+        
+        for (label, model) in [
+            (nameLabel, model.nameLabelStyle),
+            (dateLabel, model.dateLabelStyle),
+            (bodyLabel, model.bodyLabelStyle),
+            ]
+        {
+            label.applyStyleModel(model)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        fatalError("programmatic init() not supported.")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        // unfortunately, sometimes our IBOutlets are still nil at this point,
+        // so we throw this call onto the next runloop:
+        dispatch_async(dispatch_get_main_queue()) { [weak self] () -> Void in
+            if let zelf = self {
+                zelf._applyStyleModel(zelf.styleModel)
+            }
+        }
     }
 }
 
