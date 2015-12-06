8f309c236e488ef93cc629a9f153f1e7c17cbb73
Jason Pepas <jasonpepas@gmail.com>
2015-12-06 14:57:02 -0600

    working on LayoutModel

--- GutenbergTweets/GutenbergTweets/TweetViewController.swift
+++ GutenbergTweets/GutenbergTweets/TweetViewController.swift
@@ -14,6 +14,46 @@ class TweetView: UIView
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var bodyLabel: UILabel!
     
+    @IBOutlet weak var topMarginConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomMarginConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leadingMarginConstraint: NSLayoutConstraint!
+    @IBOutlet weak var trailingMarginConstraint: NSLayoutConstraint!
+
+    @IBOutlet var verticalSpacingConstraints: [NSLayoutConstraint]!
+    
+    var layoutModel: LayoutModel = LayoutModel() {
+        didSet {
+            _applyLayoutModel(layoutModel)
+        }
+    }
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        _applyLayoutModel(layoutModel)
+    }
+    
+    private func _applyLayoutModel(model: LayoutModel)
+    {
+        topMarginConstraint.constant = model.topMarginConstant
+        bottomMarginConstraint.constant = model.bottomMarginConstant
+        leadingMarginConstraint.constant = model.leadingMarginConstant
+        trailingMarginConstraint.constant = model.trailingMarginConstant
+        
+        for constraint in verticalSpacingConstraints
+        {
+            constraint.constant = model.verticalSpacingConstant
+        }
+    }
+    
+    struct LayoutModel
+    {
+        let topMarginConstant: CGFloat = 16
+        let bottomMarginConstant: CGFloat = 8
+        let leadingMarginConstant: CGFloat = 8
+        let trailingMarginConstant: CGFloat = 8
+        
+        let verticalSpacingConstant: CGFloat = 8
+    }
 }
 
 class TweetViewController: UIViewController
@@ -25,6 +65,6 @@ class TweetViewController: UIViewController
         
         tweetView.nameLabel.text = "Fred"
         tweetView.dateLabel.text = "Yesterday"
-        tweetView.bodyLabel.text = "Charlotte had seen them from her husband's room, crossing the road, and immediately running into the other, told the girls what an honour they might expect, adding:  \"I may thank you, Eliza, for this piece of civility."
+        tweetView.bodyLabel.text = "Charlotte had seen them from her husband's room, crossing the road, and immediately running into the other, told the girls what an honour they might expect, adding:  \"I may thank you, Eliza, for this piece of civility.\""
     }
 }
